b5ce98f0a7c556cf9cc66bedbee545180b512c2d
Alex Scott <codetheorist@outlook.com>
2022-02-06 16:31:23 +0000

    Bootstrap SCSS

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bootstrapSassAbstractsImports = require("vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js");
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: bootstrapSassAbstractsImports.join("\n"),
+      },
+      scss: {
+        additionalData: [...bootstrapSassAbstractsImports, ""].join(";\n"),
+      },
+    },
+  },
+};

31c81517df0f1d319101c389a5257a6477a90e7e
Alex Scott <codetheorist@outlook.com>
2022-02-06 18:29:16 +0000

    gh-pages

---
+++ js/tos.acd08ae3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["tos"],{8002:function(e,n,t){"use strict";t.r(n);var r,u,o=t("2b0e"),l=o["default"].extend({render:function(e){return e("h1","Terms of Service")}}),s=l,c=t("2877"),a=Object(c["a"])(s,r,u,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=tos.acd08ae3.js.map
---
+++ precache-manifest.52ecf0e834513d9fa6fa2785b2d73efc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8899343cabdd6239b2",
+    "url": "/css/app.6dfd4c07.css"
+  },
+  {
+    "revision": "bd46997803a1ecfb94a7eeb34d4187e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba452787d25032fff6c",
+    "url": "/js/about.a3466398.js"
+  },
+  {
+    "revision": "1d8899343cabdd6239b2",
+    "url": "/js/app.e0840aba.js"
+  },
+  {
+    "revision": "5f7d3fa8273466b72f2f",
+    "url": "/js/chunk-vendors.f761c3e2.js"
+  },
+  {
+    "revision": "eca668522a922359b4b5",
+    "url": "/js/tos.acd08ae3.js"
+  },
+  {
+    "revision": "2ac6377262ec1c73bea1ee826df668e7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
